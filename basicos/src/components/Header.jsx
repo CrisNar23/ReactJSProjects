bdac55761e9441898dcc546d1277d698f80e30dc
CrisNar23 <cristian_naranjo_15@hotmail.com>
2020-01-21 15:15:13 -0500

    Avance reemplazo de div por fragment video 37 creando un segundo componente minuto 4:29

--- basicos/src/components/Header.jsx
+++ basicos/src/components/Header.jsx
@@ -2,16 +2,8 @@ import React from 'react';
 
 function Header(){
 
-    const edad = 18;
-    let mensaje;
-    if (edad >= 18) {
-        mensaje = 'Eres mayor de edad'
-    } else{
-        mensaje = 'Eres menor de edad'
-    }
-
     return(
-        <h1>{mensaje}</h1>
+        <h1 id="encabezado" className="encabezado">Tienda Virtual</h1>
     );
 }
 
