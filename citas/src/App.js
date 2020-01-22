bd43aa61cb7393ac127bc417f2a5d12e9ded1d9c
CrisNar_23 <cristian_naranjo_15@hotmail.com>
2020-01-21 23:36:15 -0500

    Avance de proyecto hasta limpiar formulario video 57

--- citas/src/App.js
+++ citas/src/App.js
@@ -1,7 +1,14 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Formulario from './components/Formulario'
 
 function App() {
+
+  //State arreglo de citas
+  const [citas, guardarCitas] = useState([])
+
+  //Función que tome las citas actuales y agregue la nueva
+  const crearCita = cita => guardarCitas([...citas, cita])
+
   return (
     <Fragment>
       <h1>Administrador de Pacientes</h1>
@@ -9,8 +16,8 @@ function App() {
       <div className="container">
         <div className="container">
           <div className="one-half column">
-            <Formulario 
-            
+            <Formulario
+              crearCita={crearCita}
             />
           </div>
           <div className="one-half column">
