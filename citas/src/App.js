cdd9c980a7797c0b3df70da9d21da8d1190e6294
CrisNar23 <cristian_naranjo_15@hotmail.com>
2020-01-23 15:01:37 -0500

    Proyecto de citas finalizado termina en video 64

--- citas/src/App.js
+++ citas/src/App.js
@@ -1,13 +1,38 @@
-import React, { Fragment, useState } from 'react';
-import Formulario from './components/Formulario'
+import React, { Fragment, useState, useEffect } from "react";
+import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
 
 function App() {
 
+  //Citas en localStorage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales){
+    citasIniciales = [];
+  }
+
   //State arreglo de citas
-  const [citas, guardarCitas] = useState([])
+  const [citas, guardarCitas] = useState(citasIniciales);
+
+  //UseEffect para realizar ciertas operaciones cuando el state cambia
+  useEffect( () => {
+    if(citasIniciales){
+      localStorage.setItem('citas', JSON.stringify(citas));
+    }else{
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  }, [citas, citasIniciales] );
 
   //Función que tome las citas actuales y agregue la nueva
-  const crearCita = cita => guardarCitas([...citas, cita])
+  const crearCita = cita => guardarCitas([...citas, cita]);
+
+  //Función que elimina una cita por su id
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  };
+
+  //Mensaje condicional
+  const titulo = citas.length === 0 ? "No hay citas" : "Administra tus Citas";
 
   return (
     <Fragment>
@@ -16,12 +41,13 @@ function App() {
       <div className="container">
         <div className="container">
           <div className="one-half column">
-            <Formulario
-              crearCita={crearCita}
-            />
+            <Formulario crearCita={crearCita} />
           </div>
           <div className="one-half column">
-            2
+            <h2>{titulo}</h2>
+            {citas.map(cita => (
+              <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
+            ))}
           </div>
         </div>
       </div>
